ee5f40f6ba7d906ef162d7b612bc9900035ba8da
David C. Page <evildavo@gmail.com>
2021-06-12 10:53:05 +1000

    feat: Add location input form

--- website/js/app.js
+++ website/js/app.js
@@ -1,3 +1,19 @@
+
+
+
+(function setUpPage() {
+
+    // Set up location input.
+    const locInputForm = document.getElementById('loc-input-form');
+    locInputForm.addEventListener('submit', onSubmit = event => {
+        event.preventDefault();
+    }, false);
+})();
+
+
+
+
+
 // Personal API Key for OpenWeatherMap API
 
 // Event listener to add function to existing HTML DOM element
