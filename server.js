004158cf6610d63c49945acaf00d472e88dd7688
David C. Page <evildavo@gmail.com>
2021-06-20 10:34:05 +1000

    fix: Use body-parser middleware and initialize cors properly

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json);
 
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json);
-
-const cors = require('cors');
-app.use(cors);
+app.use(cors());
 
 app.use(express.static('website'));
 
