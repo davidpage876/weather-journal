4eec5d46406879a77d1cd1cdb378a3bf418eeded
David C. Page <evildavo@gmail.com>
2021-06-25 21:08:10 +1000

    fix: Missing dependency 'path'

--- server.js
+++ server.js
@@ -29,6 +29,7 @@ function UserData() {
     const express = require('express');
     const bodyParser = require('body-parser');
     const cors = require('cors');
+    const path = require('path');
 
     const app = express();
     app.use(bodyParser.urlencoded({ extended: false }));
