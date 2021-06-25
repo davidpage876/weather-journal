39906b1ffdc47d69b8628b38448249e60832e98e
David C. Page <evildavo@gmail.com>
2021-06-26 09:17:20 +1000

    feat: Add client-side get and post functions

--- website/js/app.js
+++ website/js/app.js
@@ -13,6 +13,46 @@ function OpenWeatherMap(apiKey) {
     this.getInfo = async() => { return ''; }
 };
 
+/**
+ * Performs a general GET request for the specified url resource.
+ * See server.js for specific routes.
+ * @param {string} url The url resource to request.
+ * @returns {Promise<Object>} Returns a promise containing data from the server when fulfilled.
+ */
+const getData = async (url = '') => {
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.log('Error: ', error);
+    }
+}
+
+/**
+ * Performs a general POST request for the specified url resource.
+ * See server.js for specific routes.
+ * @param {string} url The url resource to post to.
+ * @param {Object} data The data to post to the server.
+ * @returns {Promise} Returns a promise containing data from the server (where relevant) when fulfilled.
+ */
+const postData = async (url = '', data = {}) => {
+    try {
+        const response = await fetch(url, {
+            method: 'POST',
+            credentials: 'same-origin',
+            headers: {
+                'Content Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        });
+        const receivedData = await response.json();
+        return receivedData;
+    } catch (error) {
+        console.log('Error: ', error);
+    }
+}
+
 // Set up page.
 (function setUpPage() {
 
@@ -73,8 +113,6 @@ function OpenWeatherMap(apiKey) {
 
 
 
-
-// Personal API Key for OpenWeatherMap API
 
 // Event listener to add function to existing HTML DOM element
 
